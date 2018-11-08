49adfd219f893b7c6f324792c62c00567de9773c
Harriet <32721922+hkhaisty@users.noreply.github.com>
2018-11-08 14:56:08 -0500

    adds python project

---
+++ PycharmProjects/project1/Try1.py
@@ -0,0 +1,5 @@
+print("I am the great Psychic Mysterio! Input any number and I will guess what it is! ")
+
+guess = input("Enter your number: ")
+
+print("Your magic number is " + guess + " OooOoOOoOOoOOo how did I do that?!")
